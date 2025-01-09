6aceb34e4c26217e2ac043af80b1a6deac970b77
mdg0410 <levoyersteven@gmail.com>
2025-01-09 16:53:25 -0500

    Proyecto listo para conectar el fetch y controller

---
+++ Controllers/PedidoController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Karaoke.Data;
+using Karaoke.Models;
+using Karaoke.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+
+
+
+namespace Karaoke.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PedidoController : ControllerBase
+    {
+        private readonly KaraokeContext _context;
+
+        public PedidoController(KaraokeContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("EnviarPedido")]
+        public async Task<IActionResult> EnviarPedido([FromBody] List<Pedido> carrito)
+        {
+
+            Console.WriteLine("Inicio del controlador");
+
+            if (carrito == null || !carrito.Any())
+            {
+                return BadRequest("El carro de compras está vacío");
+            }
+
+            var codigoPedido = GenerarCodigoPedido(6);
+
+            //var certificados = carrito.Select(carrito => carrito.MesaCredencial).ToList();
+            //var validarCertificado = await _context.Mesas.FirstOrDefaultAsync(m => certificados.Contains(m.Credencial));
+
+            //if (validarCertificado == null)
+            //{
+            //    return NotFound("Certificado no encontrado");
+            //}
+
+            foreach (var producto in carrito)
+            {
+                var cmd = _context.Database.GetDbConnection().CreateCommand();
+                cmd.CommandText = "InsertarPedido";
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add(new SqlParameter("@IdMesa", SqlDbType.Int) { Value = producto.IdMesa });
+                cmd.Parameters.Add(new SqlParameter("@IdProducto", SqlDbType.Int) { Value = producto.IdProducto });
+                cmd.Parameters.Add(new SqlParameter("@Cantidad", SqlDbType.Int) { Value = producto.Cantidad });
+                cmd.Parameters.Add(new SqlParameter("@IdEstadoPedido", SqlDbType.Int) { Value = 1 });
+                cmd.Parameters.Add(new SqlParameter("@DetalleAdicional", SqlDbType.NVarChar, 255) { Value = (object)producto.DetalleAdicional ?? DBNull.Value });
+                cmd.Parameters.Add(new SqlParameter("@CodigoPedido", SqlDbType.NVarChar, 100) { Value = codigoPedido });
+
+                _context.Database.OpenConnection();
+                await cmd.ExecuteNonQueryAsync();
+                _context.Database.CloseConnection();
+            }
+
+            Console.WriteLine("Final del controlador");
+            return Ok("Pedido enviado correctamente");
+        }
+
+        private string GenerarCodigoPedido(int longitud)
+        {
+            const string caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            return new string(Enumerable.Repeat(caracteres, longitud)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+
+}
---
+++ ViewModel/ProductoViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Karaoke.ViewModels
+{
+    public class ProductoViewModel
+    {
+        public int IdMesa { get; set; }
+        public int IdProducto { get; set; }
+        public int Cantidad { get; set; }
+        public string? DetalleAdicional { get; set; }
+    }
+}
